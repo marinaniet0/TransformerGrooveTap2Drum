43efdf6574e5a944b54e90ba3f12cc3313bf8edd
marinaniet0 <marinanietogimenez@gmail.com>
2021-06-10 04:59:31 +0200

    Modified code to match unified params and saving model in BaseGroove repo

--- model/dataset.py
+++ model/dataset.py
@@ -10,21 +10,21 @@ import wandb
 import numpy as np
 
 
-sys.path.insert(1, '../../hvo_sequence/')
-sys.path.insert(1, '../hvo_sequence/')
+sys.path.insert(1, "../../hvo_sequence/")
+sys.path.insert(1, "../hvo_sequence/")
 from hvo_sequence.hvo_seq import HVO_Sequence
 from hvo_sequence.drum_mappings import ROLAND_REDUCED_MAPPING
 
 default_subset_info = {
-    "metadata_csv_filename": 'metadata.csv',
-    "hvo_pickle_filename": 'hvo_sequence_data.obj',
+    "metadata_csv_filename": "metadata.csv",
+    "hvo_pickle_filename": "hvo_sequence_data.obj",
     "pickle_source_path": "../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.2"
                           "/Processed_On_17_05_2021_at_22_32_hrs",
-    "subset_name": 'GrooveMIDI_processed_train'
+    "subset_name": "GrooveMIDI_processed_train"
 }
 
 default_tap_params = {
-    "tapped_sequence_voice": 'HH_CLOSED',
+    "tapped_sequence_voice": "HH_CLOSED",
     "tapped_sequence_collapsed": False,
     "tapped_sequence_velocity_mode": 1,
     "tapped_sequence_offset_mode": 3
@@ -68,7 +68,7 @@ def process_dataset(subset, metadata, max_len, tappify_params):
                 hvo_seq.beat_type = metadata.loc[idx].at["beat_type"]
 
                 pad_count = max(max_len - hvo_seq.hvo.shape[0], 0)
-                hvo_seq.hvo = np.pad(hvo_seq.hvo, ((0, pad_count), (0, 0)), 'constant')
+                hvo_seq.hvo = np.pad(hvo_seq.hvo, ((0, pad_count), (0, 0)), "constant")
                 hvo_seq.hvo = hvo_seq.hvo[:max_len, :]  # in case seq exceeds max len
                 hvo_sequences.append(hvo_seq)
                 flat_seq = hvo_seq.flatten_voices(voice_idx=tapped_voice_idx,
@@ -79,7 +79,7 @@ def process_dataset(subset, metadata, max_len, tappify_params):
                 outputs.append(hvo_seq.hvo)
 
     # Load data onto device
-    dev = 'cuda' if torch.cuda.is_available() else 'cpu'
+    dev = "cuda" if torch.cuda.is_available() else "cpu"
     inputs = torch.FloatTensor(inputs).to(dev)
     outputs = torch.FloatTensor(outputs).to(dev)
 
@@ -130,10 +130,10 @@ class GrooveMidiDataset(Dataset):
                  **kwargs):
 
         # Get kwargs
-        max_len = kwargs.get('max_len', default_max_len)
-        tappify_params = kwargs.get('tappify_params', default_tap_params)
+        max_len = kwargs.get("max_len", default_max_len)
+        tappify_params = kwargs.get("tappify_params", default_tap_params)
 
-        print('Loading dataset...')
+        print("Loading dataset...")
 
         # Loading metadata onto DataFrame
         metadata = pd.read_csv(os.path.join(subset_info["pickle_source_path"], subset_info["subset"],
@@ -143,7 +143,7 @@ class GrooveMidiDataset(Dataset):
         self.inputs, self.outputs, self.sequences = process_dataset(subset, metadata, max_len, tappify_params)
 
         # wandb.config.update({"set_length": len(self.sequences)})
-        print('Dataset loaded\n')
+        print("Dataset loaded\n")
 
     def __len__(self):
         return len(self.sequences)
--- model/train_tap2drum.py
+++ model/train_tap2drum.py
@@ -3,30 +3,31 @@ import wandb
 import numpy as np
 import pandas as pd
 import torch
+from torch.utils.data import DataLoader
 from dataset import GrooveMidiDataset, process_dataset
 
-sys.path.insert(1, '../../BaseGrooveTransformers/')
-sys.path.insert(1, '../BaseGrooveTransformers/')
+sys.path.insert(1, "../../BaseGrooveTransformers/")
+sys.path.insert(1, "../BaseGrooveTransformers/")
 from models.train import *
 
-sys.path.insert(1, '../../GrooveEvaluator/')
-sys.path.insert(1, '../GrooveEvaluator/')
+sys.path.insert(1, "../../GrooveEvaluator/")
+sys.path.insert(1, "../GrooveEvaluator/")
 from GrooveEvaluator.evaluator import Evaluator
 
-sys.path.insert(1, '../../preprocessed_dataset/')
-sys.path.insert(1, '../preprocessed_dataset/')
+sys.path.insert(1, "../../preprocessed_dataset/")
+sys.path.insert(1, "../preprocessed_dataset/")
 from Subset_Creators.subsetters import GrooveMidiSubsetter
 
 #import os
-#os.environ['WANDB_MODE']='offline'
-sys.path.insert(1, '../../hvo_sequence/')
-sys.path.insert(1, '../hvo_sequence/')
+#os.environ["WANDB_MODE"]="offline"
+sys.path.insert(1, "../../hvo_sequence/")
+sys.path.insert(1, "../hvo_sequence/")
 from hvo_sequence.drum_mappings import ROLAND_REDUCED_MAPPING
 
 if __name__ == "__main__":
 
     hyperparameter_defaults = dict(
-        optimizer_algorithm='sgd',
+        optimizer_algorithm="sgd",
         d_model=128,
         n_heads=8,
         dropout=0.1,
@@ -35,85 +36,98 @@ if __name__ == "__main__":
         learning_rate=1e-3,
         batch_size=64,
         dim_feedforward=1280,
-        epochs=100,
+        epochs=1,
         lr_scheduler_step_size=30,
         lr_scheduler_gamma=0.1
     )
 
-    wandb_run = wandb.init(config=hyperparameter_defaults)
-    
-    save_info = {
-        'checkpoint_path': '../results/',
-        'checkpoint_save_str': '../results/transformer_groove_tap2drum-epoch-{}',
-        'df_path': '../results/losses_df/'
-    }
+    wandb_run = wandb.init(config=hyperparameter_defaults, project="tap2drum")
 
-    filters = {
-        "beat_type": ["beat"],
-        "time_signature": ["4-4"]
-    }
-
-    subset_info = {
-        "pickle_source_path": '../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.4'
-                              '/Processed_On_09_06_2021_at_12_41_hrs',
-        "subset": 'GrooveMIDI_processed_train',
-        "metadata_csv_filename": 'metadata.csv',
-        "hvo_pickle_filename": 'hvo_sequence_data.obj',
-        "filters": filters
-    }
-
-    tap_params = {
-        "tapped_sequence_voice": 'HH_CLOSED',
-        "tapped_sequence_collapsed": False,
-        "tapped_sequence_velocity_mode": 1,
-        "tapped_sequence_offset_mode": 3
-    }
-
-    seq_max_len = 32
-
-    # TRANSFORMER MODEL PARAMETERS
-    model_parameters = {
-        'optimizer': wandb.config.optimizer_algorithm,
-        'd_model': wandb.config.d_model,
-        'n_heads': wandb.config.n_heads,
-        'dim_feedforward': wandb.config.dim_feedforward,
-        'dropout': wandb.config.dropout,
-        'num_encoder_layers': wandb.config.num_encoder_layers,
-        'num_decoder_layers': wandb.config.num_decoder_layers,
-        'max_len': 32,
-        'embedding_size_src': 27,
-        'embedding_size_tgt': 27,
-        'device': 'cuda' if torch.cuda.is_available() else 'cpu'
-    }
-
-    # TRAINING PARAMETERS
-    training_parameters = {
-        'learning_rate': wandb.config.learning_rate,
-        'batch_size': wandb.config.batch_size,
-        'lr_scheduler_step_size': wandb.config.lr_scheduler_step_size,
-        'lr_scheduler_gamma': wandb.config.lr_scheduler_gamma
+    params = {
+        "model": {
+            "optimizer": wandb.config.optimizer_algorithm,
+            "d_model": wandb.config.d_model,
+            "n_heads": wandb.config.n_heads,
+            "dim_feedforward": wandb.config.dim_feedforward,
+            "dropout": wandb.config.dropout,
+            "num_encoder_layers": wandb.config.num_encoder_layers,
+            "num_decoder_layers": wandb.config.num_decoder_layers,
+            "max_len": 32,
+            "embedding_size_src": 27,
+            "embedding_size_tgt": 27,
+            "device": "cuda" if torch.cuda.is_available() else "cpu"
+        },
+        "training": {
+            "learning_rate": wandb.config.learning_rate,
+            "batch_size": wandb.config.batch_size,
+            "lr_scheduler_step_size": wandb.config.lr_scheduler_step_size,
+            "lr_scheduler_gamma": wandb.config.lr_scheduler_gamma
+        },
+        "dataset": {
+            "pickle_source_path": "../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.4"
+                                  "/Processed_On_09_06_2021_at_12_41_hrs",
+            "subset": "GrooveMIDI_processed_train",
+            "metadata_csv_filename": "metadata.csv",
+            "hvo_pickle_filename": "hvo_sequence_data.obj",
+            "filters": {
+                "beat_type": ["beat"],
+                "time_signature": ["4-4"]
+            },
+            "max_len": 32
+        },
+        "tappify_params": {
+            "tapped_sequence_voice": "HH_CLOSED",
+            "tapped_sequence_collapsed": False,
+            "tapped_sequence_velocity_mode": 1,
+            "tapped_sequence_offset_mode": 3
+        },
+        "load_model": None  # if we don't want to load any model, set to None
+        #"load_model": {
+        #    "location": "local",
+        #    "dir": "./wandb/run-20210609_162149-1tsi1g1n/files/saved_models/",
+        #    "file_pattern": "transformer_run_{}_Epoch_{}.Model"
+        #}
+        #"load_model": {
+        #    "location": "wandb",
+        #    "dir": "marinaniet0/tap2drum/1tsi1g1n/",
+        #    "file_pattern": "saved_models/transformer_run_{}_Epoch_{}.Model",
+        #    "epoch": 51,
+        #    "run": "1tsi1g1n"
+        #}
     }
 
     # PYTORCH LOSS FUNCTIONS
     BCE_fn = torch.nn.BCEWithLogitsLoss()
     MSE_fn = torch.nn.MSELoss()
 
-    # DATASET LOADING FOR TRAINING
-    _, subset_list = GrooveMidiSubsetter(pickle_source_path=subset_info["pickle_source_path"],
-                                         subset=subset_info["subset"],
-                                         hvo_pickle_filename=subset_info["hvo_pickle_filename"],
-                                         list_of_filter_dicts_for_subsets=[filters]).create_subsets()
+    model, optimizer, scheduler, ep = initialize_model(params)
+    wandb.watch(model)
 
-    gmd = GrooveMidiDataset(subset=subset_list[0], subset_info=subset_info, max_len=seq_max_len,
-                            tappify_params=tap_params)
-    dataloader = DataLoader(gmd, batch_size=training_parameters['batch_size'], shuffle=True)
+    # DATASET LOADING FOR TRAINING
+    _, subset_list = GrooveMidiSubsetter(pickle_source_path=params["dataset"]["pickle_source_path"],
+                                         subset=params["dataset"]["subset"],
+                                         hvo_pickle_filename=params["dataset"]["hvo_pickle_filename"],
+                                         list_of_filter_dicts_for_subsets=[params["dataset"]["filters"]]).create_subsets()
+
+    gmd = GrooveMidiDataset(subset=subset_list[0], subset_info=params["dataset"],
+                            tappify_params=params["tappify_params"], max_len=params["dataset"]["max_len"])
+
+    dataloader = DataLoader(gmd, batch_size=params["training"]["batch_size"], shuffle=True)
 
     # EVALUATOR
+    styles = ["hiphop", "funk", "reggae", "soul", "latin", "jazz", "pop", "afrobeat", "highlife", "punk", "rock"]
+
+    list_of_filter_dicts_for_subsets = []
+    for style in styles:
+        list_of_filter_dicts_for_subsets.append(
+           {"style_primary": [style], "beat_type": ["beat"], "time_signature": ["4-4"]}
+        )
+
     evaluator = Evaluator(
-        pickle_source_path=subset_info["pickle_source_path"],
-        set_subfolder=subset_info["subset"],
-        hvo_pickle_filename=subset_info["hvo_pickle_filename"],
-        list_of_filter_dicts_for_subsets=[filters],
+        pickle_source_path=params["dataset"]["pickle_source_path"],
+        set_subfolder=params["dataset"]["subset"],
+        hvo_pickle_filename=params["dataset"]["hvo_pickle_filename"],
+        list_of_filter_dicts_for_subsets=list_of_filter_dicts_for_subsets,
         max_hvo_shape=(32, 27),
         n_samples_to_use=1024,
         n_samples_to_synthesize_visualize_per_subset=10,
@@ -122,15 +136,12 @@ if __name__ == "__main__":
         analyze_global_features=True
     )
     evaluator_subset = evaluator.get_ground_truth_hvo_sequences()
-    metadata = pd.read_csv(os.path.join(subset_info["pickle_source_path"], subset_info["subset"],
-                                            subset_info["metadata_csv_filename"]))
-    eval_inputs, _, _ = process_dataset(evaluator_subset, metadata=metadata, max_len=seq_max_len,
-                                                               tappify_params=tap_params)
+    metadata = pd.read_csv(os.path.join(params["dataset"]["pickle_source_path"], params["dataset"]["subset"],
+                                            params["dataset"]["metadata_csv_filename"]))
+    eval_inputs, _, _ = process_dataset(evaluator_subset, metadata=metadata, max_len=params["dataset"]["max_len"],
+                                                               tappify_params=params["tappify_params"])
 
-    model, optimizer, scheduler, ep = initialize_model(model_parameters, training_parameters, save_info, load_from_checkpoint=False)
-    wandb.watch(model)
-
-    epoch_save_div = 1
+    epoch_save_div = 2
     eps = wandb.config.epochs
 
     # GENERATE FREQUENCY LOG ARRAYS
@@ -153,7 +164,7 @@ if __name__ == "__main__":
             print(f"Epoch {ep}\n-------------------------------")
             train_loop(dataloader=dataloader, groove_transformer=model, opt=optimizer, scheduler=scheduler, epoch=ep,
                        loss_fn=calculate_loss, bce_fn=BCE_fn, mse_fn=MSE_fn, save=save_model,
-                       device=model_parameters['device'])
+                       device=params["model"]["device"])
             print("-------------------------------\n")
 
             eval_pred = torch.cat(model.predict(eval_inputs, use_thres=True, thres=0.5), dim=2)
@@ -178,11 +189,12 @@ if __name__ == "__main__":
 
                 # Heatmaps
                 heatmaps_global_features = evaluator.get_wandb_logging_media(
-                    sf_paths=['../../hvo_sequence/hvo_sequence/soundfonts/Standard_Drum_Kit.sf2'])
+                    sf_paths=["../../hvo_sequence/hvo_sequence/soundfonts/Standard_Drum_Kit.sf2"])
                 if len(heatmaps_global_features.keys()) > 0:
                     wandb.log(heatmaps_global_features, commit=False)
 
             evaluator.dump(path="misc/evaluator_run_{}_Epoch_{}.Eval".format(wandb_run.name, ep))
+            wandb.log({"epoch": ep})
 
     finally:
         wandb.finish()
