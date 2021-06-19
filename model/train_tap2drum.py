75c3f04e4b3f009778cf622e81cadfdb5569d695
marinaniet0 <marinanietogimenez@gmail.com>
2021-06-19 16:23:07 +0200

    added test set evaluator

--- model/train_tap2drum.py
+++ model/train_tap2drum.py
@@ -60,10 +60,22 @@ if __name__ == "__main__":
             "learning_rate": wandb.config.learning_rate,
             "batch_size": wandb.config.batch_size
         },
-        "dataset": {
+        "train_dataset": {
             "pickle_source_path": "../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.5"
                                   "/Processed_On_14_06_2021_at_14_26_hrs",
             "subset": "GrooveMIDI_processed_train",
+            "metadata_csv_filename": "metadata.csv",
+            "hvo_pickle_filename": "hvo_sequence_data.obj",
+            "filters": {
+                "beat_type": ["beat"],
+                "time_signature": ["4-4"]
+            },
+            "max_len": 32
+        },
+        "test_dataset": {
+            "pickle_source_path": "../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.5"
+                                  "/Processed_On_14_06_2021_at_14_26_hrs",
+            "subset": "GrooveMIDI_processed_test",
             "metadata_csv_filename": "metadata.csv",
             "hvo_pickle_filename": "hvo_sequence_data.obj",
             "filters": {
@@ -102,13 +114,13 @@ if __name__ == "__main__":
     wandb.watch(model)
 
     # DATASET LOADING FOR TRAINING
-    _, subset_list = GrooveMidiSubsetter(pickle_source_path=params["dataset"]["pickle_source_path"],
-                                         subset=params["dataset"]["subset"],
-                                         hvo_pickle_filename=params["dataset"]["hvo_pickle_filename"],
-                                         list_of_filter_dicts_for_subsets=[params["dataset"]["filters"]]).create_subsets()
+    _, subset_list = GrooveMidiSubsetter(pickle_source_path=params["train_dataset"]["pickle_source_path"],
+                                         subset=params["train_dataset"]["subset"],
+                                         hvo_pickle_filename=params["train_dataset"]["hvo_pickle_filename"],
+                                         list_of_filter_dicts_for_subsets=[params["train_dataset"]["filters"]]).create_subsets()
 
-    gmd = GrooveMidiDatasetTap2Drum(subset=subset_list[0], subset_info=params["dataset"],
-                                    tappify_params=params["tappify_params"], max_len=params["dataset"]["max_len"])
+    gmd = GrooveMidiDatasetTap2Drum(subset=subset_list[0], subset_info=params["train_dataset"],
+                                    tappify_params=params["tappify_params"], max_len=params["train_dataset"]["max_len"])
 
     dataloader = DataLoader(gmd, batch_size=params["training"]["batch_size"], shuffle=True)
 
@@ -125,10 +137,11 @@ if __name__ == "__main__":
                {"style_primary": [style], "beat_type": ["beat"], "time_signature": ["4-4"]}
             )
 
-        evaluator = Evaluator(
-            pickle_source_path=params["dataset"]["pickle_source_path"],
-            set_subfolder=params["dataset"]["subset"],
-            hvo_pickle_filename=params["dataset"]["hvo_pickle_filename"],
+        # TRAIN EVALUATOR
+        train_evaluator = Evaluator(
+            pickle_source_path=params["train_dataset"]["pickle_source_path"],
+            set_subfolder=params["train_dataset"]["subset"],
+            hvo_pickle_filename=params["train_dataset"]["hvo_pickle_filename"],
             list_of_filter_dicts_for_subsets=list_of_filter_dicts_for_subsets,
             max_hvo_shape=(32, 27),
             n_samples_to_use=2048,
@@ -137,11 +150,35 @@ if __name__ == "__main__":
             analyze_heatmap=True,
             analyze_global_features=True
         )
-        evaluator_subset = evaluator.get_ground_truth_hvo_sequences()
-        metadata = pd.read_csv(os.path.join(params["dataset"]["pickle_source_path"], params["dataset"]["subset"],
-                                                params["dataset"]["metadata_csv_filename"]))
-        eval_inputs, _, _ = process_dataset(evaluator_subset, metadata=metadata, max_len=params["dataset"]["max_len"],
-                                                                   tappify_params=params["tappify_params"])
+        train_evaluator_subset = train_evaluator.get_ground_truth_hvo_sequences()
+        metadata_train = pd.read_csv(os.path.join(params["train_dataset"]["pickle_source_path"],
+                                                  params["train_dataset"]["subset"],
+                                                  params["train_dataset"]["metadata_csv_filename"]))
+        train_eval_inputs, _, _ = process_dataset(train_evaluator_subset, metadata=metadata_train,
+                                                  max_len=params["train_dataset"]["max_len"],
+                                                  tappify_params=params["tappify_params"])
+
+        # TEST EVALUATOR
+        test_evaluator = Evaluator(
+            pickle_source_path=params["test_dataset"]["pickle_source_path"],
+            set_subfolder=params["test_dataset"]["subset"],
+            hvo_pickle_filename=params["test_dataset"]["hvo_pickle_filename"],
+            list_of_filter_dicts_for_subsets=list_of_filter_dicts_for_subsets,
+            max_hvo_shape=(32, 27),
+            n_samples_to_use=2048,
+            n_samples_to_synthesize_visualize_per_subset=10,
+            disable_tqdm=False,
+            analyze_heatmap=True,
+            analyze_global_features=True
+        )
+        test_evaluator_subset = test_evaluator.get_ground_truth_hvo_sequences()
+        metadata_test = pd.read_csv(os.path.join(params["test_dataset"]["pickle_source_path"],
+                                                 params["test_dataset"]["subset"],
+                                                 params["test_dataset"]["metadata_csv_filename"]))
+        test_eval_inputs, _, _ = process_dataset(train_evaluator_subset, metadata=metadata_test,
+                                                  max_len=params["test_dataset"]["max_len"],
+                                                  tappify_params=params["tappify_params"])
+
 
     # GENERATE FREQUENCY LOG ARRAYS
     # epoch_save_partial, epoch_save_all = eval_log_freq(total_epochs=eps, initial_epochs_lim=10, initial_step_partial=2,
@@ -166,31 +203,59 @@ if __name__ == "__main__":
 
                 if i in epoch_save_partial or i in epoch_save_all:
 
-                    eval_pred = torch.cat(model.predict(eval_inputs, use_thres=True, thres=0.5), dim=2)
-                    eval_pred_hvo_array = eval_pred.cpu().detach().numpy()
-                    evaluator.add_predictions(eval_pred_hvo_array)
-                    evaluator.identifier = 'Test_Epoch_{}'.format(ep)
+                    # EVAL TRAIN
+
+                    train_eval_pred = torch.cat(model.predict(train_eval_inputs, use_thres=True, thres=0.5), dim=2)
+                    train_eval_pred_hvo_array = train_eval_pred.cpu().detach().numpy()
+                    train_evaluator.add_predictions(train_eval_pred_hvo_array)
+                    train_evaluator.identifier = 'Train_Epoch_{}'.format(ep)
 
                     # Evaluate
-                    acc_h = evaluator.get_hits_accuracies(drum_mapping=ROLAND_REDUCED_MAPPING)
-                    mse_v = evaluator.get_velocity_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
-                    mse_o = evaluator.get_micro_timing_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
-                    rhythmic_distances = evaluator.get_rhythmic_distances()
+                    acc_h = train_evaluator.get_hits_accuracies(drum_mapping=ROLAND_REDUCED_MAPPING)
+                    mse_v = train_evaluator.get_velocity_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
+                    mse_o = train_evaluator.get_micro_timing_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
+                    # rhythmic_distances = train_evaluator.get_rhythmic_distances()
 
                     # Log
                     wandb.log(acc_h, commit=False)
                     wandb.log(mse_v, commit=False)
                     wandb.log(mse_o, commit=False)
-                    wandb.log(rhythmic_distances, commit=False)
+                    # wandb.log(rhythmic_distances, commit=False)
+
+                    # EVAL TEST
+
+                    test_eval_pred = torch.cat(model.predict(test_eval_inputs, use_thres=True, thres=0.5), dim=2)
+                    test_eval_pred_hvo_array = test_eval_pred.cpu().detach().numpy()
+                    test_evaluator.add_predictions(test_eval_pred_hvo_array)
+                    test_evaluator.identifier = 'Test_Epoch_{}'.format(ep)
+
+                    # Evaluate
+                    acc_h = test_evaluator.get_hits_accuracies(drum_mapping=ROLAND_REDUCED_MAPPING)
+                    mse_v = test_evaluator.get_velocity_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
+                    mse_o = test_evaluator.get_micro_timing_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
+                    # rhythmic_distances = test_evaluator.get_rhythmic_distances()
+
+                    # Log
+                    wandb.log(acc_h, commit=False)
+                    wandb.log(mse_v, commit=False)
+                    wandb.log(mse_o, commit=False)
+                    # wandb.log(rhythmic_distances, commit=False)
 
                     if i in epoch_save_all:
-                        # Heatmaps
-                        heatmaps_global_features = evaluator.get_wandb_logging_media(
+                        # Heatmaps train
+                        heatmaps_global_features = train_evaluator.get_wandb_logging_media(
                             sf_paths=["../../hvo_sequence/hvo_sequence/soundfonts/Standard_Drum_Kit.sf2"])
                         if len(heatmaps_global_features.keys()) > 0:
                             wandb.log(heatmaps_global_features, commit=False)
 
-                    evaluator.dump(path="misc/evaluator_run_{}_Epoch_{}.Eval".format(wandb_run.name, ep))
+                        # Heatmaps test
+                        heatmaps_global_features = test_evaluator.get_wandb_logging_media(
+                            sf_paths=["../../hvo_sequence/hvo_sequence/soundfonts/Standard_Drum_Kit.sf2"])
+                        if len(heatmaps_global_features.keys()) > 0:
+                            wandb.log(heatmaps_global_features, commit=False)
+
+                    train_evaluator.dump(path="misc/train_set_evaluator_run_{}_Epoch_{}.Eval".format(wandb_run.name, ep))
+                    test_evaluator.dump(path="misc/test_set_evaluator_run_{}_Epoch_{}.Eval".format(wandb_run.name, ep))
 
             wandb.log({"epoch": ep})
 
